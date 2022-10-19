19af7c7ac98313635c69f2b049497ba2589130cd
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-10-19 15:48:32 -0300

    Initial Commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'poppins, sans-serif',
+      }
+    },
+  },
+  plugins: [],
+}
