75e7df2c691db8b71a5dd218507f457f061d94a8
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-10-19 21:19:25 -0300

    Config: tailwind theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,40 @@ module.exports = {
     './src/**/*.tsx'
   ],
   theme: {
+    fontSize: {
+      xs: 12,
+      sm: 14,
+      md: 16,
+      lg: 18,
+      xl: 24,
+      '2xl': 32,
+      '4xl': 44
+    },
+    colors: {
+      transparent: 'transparent',
+      white: '#FFF',
+      black: '#000',
+      red: '#F04439',
+      blue: '#3B5998',
+
+      gray: {
+        100: '#EEF4F6',
+        200: '#C4C4CC',
+        400: '#7C7C8A',
+        800: '#343C3F',
+        900: '#121214',
+        github: '#1C2227',
+      },
+      orange: {
+        300: '#FFA33D',
+        500: '#FE9016',
+        700: '#E28926',
+      },
+      cyan: {
+        500: '#72D6FD',
+        700: '#61B8D9'
+      },
+    },
     extend: {
       fontFamily: {
         sans: 'poppins, sans-serif',
