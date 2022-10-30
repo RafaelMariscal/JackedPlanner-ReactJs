ac10e06bbe7134552a9f39c606e1c927773fd689
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-10-30 15:11:30 -0300

    Feat: On clicking description card, its trigger exercise description popover.

--- src/components/Dashboard/ExerciseCard.tsx
+++ src/components/Dashboard/ExerciseCard.tsx
@@ -2,14 +2,14 @@ import { ReactNode } from "react";
 import { clsx } from 'clsx'
 
 interface ExerciseCardRootProps {
-  selected?: boolean;
-  showDescription?: boolean;
-  done?: boolean;
+  selected: boolean;
+  done: boolean;
+  index: number;
   className?: string
   children: ReactNode
 }
 
-function Root({ selected = false, showDescription = false, done = false, className, children }: ExerciseCardRootProps) {
+function Root({ selected = false, done = false, index, className, children }: ExerciseCardRootProps) {
   return (
     <div className={clsx(
       'flex items-center gap-2 font-semibold text-gray-800 select-none',
@@ -18,15 +18,13 @@ function Root({ selected = false, showDescription = false, done = false, classNa
       '[&>span]:text-gray-100 [&>span]:text-xs [&>span]:block [&>span]:h-6',
       {
         '[&>div]:border-orange-500': selected === true,
-        '[&_#description]:bg-orange-500': showDescription === true,
         '[&_div]:bg-cyan-500': done === true,
-        '[&_div]:bg-gray-100': done === false,
       },
       className
     )}
     >
       {children}
-    </div>
+    </div >
   )
 }
 
@@ -36,7 +34,7 @@ interface ExerciseCardIndexProps {
 
 function Index({ index }: ExerciseCardIndexProps) {
   return (
-    <div className="w-9 justify-center">
+    <div className="w-9 justify-center bg-gray-100">
       <span>{index}</span>
     </div>
   )
@@ -44,26 +42,14 @@ function Index({ index }: ExerciseCardIndexProps) {
 
 interface ExerciseCardNameProps {
   name: string;
-  description?: string;
-  showDescription?: boolean
 }
 
-function Name({ name, description, showDescription = false }: ExerciseCardNameProps) {
+function Name({ name, }: ExerciseCardNameProps) {
   return (
-    <div className="flex-col justify-start gap-2">
-      <span className="self-start py-[.375rem] mx-[.875rem] leading-tight">
+    <div className="w-full max-w-xs bg-gray-100 relative">
+      <span className="mx-[.875rem] block w-full">
         {name}
       </span>
-      <div className={clsx(
-        "z-10 w-full h-fit px-[.875rem] py-1 text-sm flex flex-col justify-start rounded-lg",
-        {
-          'hidden': showDescription === false
-        }
-      )}
-      >
-        <span className="block mb-1 underline">Description:</span>
-        <p>{description}</p>
-      </div>
     </div >
   )
 }
@@ -74,7 +60,7 @@ interface ExerciseCardSetsProps {
 
 function Sets({ sets }: ExerciseCardSetsProps) {
   return (
-    <div className="w-9 justify-center">
+    <div className="w-9 justify-center bg-gray-100">
       <span>{sets}</span>
     </div>
   )
@@ -86,19 +72,58 @@ interface ExerciseCardRepsProps {
 
 function Reps({ reps }: ExerciseCardRepsProps) {
   return (
-    <div className="w-9 justify-center">
+    <div className="w-9 justify-center bg-gray-100">
       <span>{reps}</span>
     </div>
   )
 }
 
+interface ExerciseCardDescriptionCardProps {
+  showDescription: boolean
+}
+
+function DescriptionCard({ showDescription }: ExerciseCardDescriptionCardProps) {
+
+  return (
+    <div className={clsx(
+      "w-9 h-9 flex rounded-lg justify-center cursor-pointer bg-gray-100",
+      {
+        'bg-orange-500': showDescription === true
+      }
+    )}
+    >
+      <img src="/src/assets/icons/Dashboard/Description.svg" alt=""
+        className="w-4"
+      />
+    </div >
+  )
+}
+
 interface ExerciseCardDescriptionProps {
+  description: string
+}
+
+function Description({ description }: ExerciseCardDescriptionProps) {
+  return (
+    <div className="
+        px-[.875rem] py-1 text-sm
+        flex flex-col justify-start rounded-lg 
+        bg-gray-100 border-2 border-gray-800
+      "
+    >
+      <span className="mb-1 underline">Description:</span>
+      <p>{description}</p>
+    </div>
+  )
+}
+
+interface ExerciseCardTodoProps {
   selected?: boolean
 }
 
-function Description({ selected = false }: ExerciseCardDescriptionProps) {
+function Todo({ selected = false }: ExerciseCardTodoProps) {
   return (
-    <div id="description" className={clsx(
+    <div className={clsx(
       "w-9 justify-center",
 
     )}>
@@ -108,6 +133,7 @@ function Description({ selected = false }: ExerciseCardDescriptionProps) {
     </div>
   )
 }
+
 
 /* 
         [ ] TO DO CARD
@@ -119,5 +145,5 @@ function Description({ selected = false }: ExerciseCardDescriptionProps) {
 
 
 export const ExerciseCard = {
-  Root, Index, Name, Sets, Reps, Description
+  Root, Index, Name, Sets, Reps, DescriptionCard, Description
 }
--- src/components/Dashboard/WorkoutSection.tsx
+++ src/components/Dashboard/WorkoutSection.tsx
@@ -1,25 +1,67 @@
 import { useId, useState } from "react";
 import DashboardCard from "./DashboardCard";
 import { ExerciseCard } from "./ExerciseCard";
+import * as Popover from '@radix-ui/react-popover';
 
 export function WorkoutSection() {
-  const [isExerciseSelected, setisExerciseSelected] = useState(true)
+  const [isExerciseSelected, setisExerciseSelected] = useState(false)
   const [isExerciseDone, setIsExerciseDone] = useState(false)
-  const [isDescriptionSelected, setisDescriptionSelected] = useState(false)
+
+  const ExerciseList = [
+    { name: 'Crucifixo com Cabos', index: 1, description: 'Description 1 testando' },
+    { name: 'Crossover (Bi-articulado)', index: 2, description: 'Description 2 testando' },
+  ]
+
   return (
     <DashboardCard
       title="Workout Section:"
       subtitle="Chest / Shoulders"
     >
       <div className="flex flex-col gap-2">
-        <ExerciseCard.Root key={useId()} selected={isExerciseSelected} done={isExerciseDone} showDescription={isDescriptionSelected} >
-          <ExerciseCard.Index index={1} />
-          <ExerciseCard.Name name={'Exercise Name'} description={'test'} showDescription={isDescriptionSelected} />
-          <ExerciseCard.Sets sets={3} />
-          <span>x</span>
-          <ExerciseCard.Reps reps={20} />
-          <ExerciseCard.Description selected />
-        </ExerciseCard.Root>
+        {
+          ExerciseList.map(exercise => {
+            const [showDescription, setshowDescription] = useState(false)
+            function handleDescriptionCardClick(isOpen: Boolean) {
+              if (isOpen) {
+                return setshowDescription(true)
+              } else {
+                return setshowDescription(false)
+              }
+            }
+            return (
+              <Popover.Root key={useId()} onOpenChange={(isOpen) => handleDescriptionCardClick(isOpen)}>
+                <ExerciseCard.Root key={useId()}
+                  selected={isExerciseSelected}
+                  done={isExerciseDone}
+                  index={exercise.index}
+                  className="relative"
+                >
+                  <ExerciseCard.Index index={exercise.index} />
+                  <ExerciseCard.Name name={exercise.name} />
+                  <Popover.Anchor className="absolute ml-[2.75rem] mt-2" />
+                  <Popover.Portal>
+                    <Popover.Content align="start">
+                      <ExerciseCard.Description description={exercise.description} />
+                    </Popover.Content>
+                  </Popover.Portal>
+
+                  <ExerciseCard.Sets sets={3} />
+
+                  <span>x</span>
+
+                  <ExerciseCard.Reps reps={20} />
+
+                  <Popover.Trigger className="closed"
+                    onClick={() => setshowDescription(!showDescription)}>
+                    <ExerciseCard.DescriptionCard showDescription={showDescription} />
+                  </Popover.Trigger>
+
+                </ExerciseCard.Root>
+              </Popover.Root>
+            )
+          })
+        }
+
       </div>
 
     </DashboardCard>
