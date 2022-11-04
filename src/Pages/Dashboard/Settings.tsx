e6de22c5934284aa2ed8fd1e6caf26039e00a950
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-11-04 11:57:48 -0300

    Start: SettingsPage Front-end

--- src/Pages/Dashboard/Settings.tsx
+++ src/Pages/Dashboard/Settings.tsx
@@ -1,6 +1,25 @@
+import { ExercisePlan } from "../../components/Dashboard/ExercisePlan";
+import { ImageCard } from "../../components/Dashboard/ImageCard";
+import { NotesHistory } from "../../components/Dashboard/NotesHistory";
+import { PersonalNotes } from "../../components/Dashboard/PersonalNotes";
+import { PlannerController } from "../../components/Dashboard/PlannerController";
 
 export function Settings() {
   return (
-    <div>Settings</div>
+    <div className="h-full flex gap-4" >
+
+      <div className="h-full w-full max-w-3xl flex flex-col gap-4 relative" >
+        <div className="w-full flex justify-between gap-4">
+          <PlannerController price={4.99} />
+        </div>
+
+        <PersonalNotes history />
+
+        <ExercisePlan />
+      </div>
+
+      <ImageCard variant="girl" />
+
+    </div>
   )
 }
