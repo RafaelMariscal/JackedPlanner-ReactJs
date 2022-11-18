5d6ab832ca20859d708deb0cc8e0d46ff5801c9f
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-11-18 16:04:55 -0300

    Add: PlannerProps types

---
+++ src/@types/PlannerProps.ts
@@ -0,0 +1,33 @@
+import { Timestamp } from "firebase/firestore";
+
+type ScheduleLabels = "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "rest"
+
+export interface PlannerProps {
+  name: string
+  startDate: Date | Timestamp
+  schedule: ScheduleLabels[]
+  split: SplitProps
+}
+
+export interface SplitProps {
+  title: string;
+  startDate: Date | Timestamp;
+  schedule: ScheduleProps
+  exercises: ExerciseProps[]
+}
+
+export interface ScheduleProps {
+  scheduledDate: Date | Timestamp
+  splitKey: ScheduleLabels
+}
+export interface ExerciseProps {
+  index: number
+  name: string
+  weightUnd: string
+  description: string
+  sets: number
+  reps: number
+  setsWeight: number[]
+  liftedReps: number[]
+  liftedWeight: number[]
+}
